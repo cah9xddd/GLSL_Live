d3d4f77adbed01e490569e88c89b99b6f98a94c9
cah9xdd <cah9xd@gmail.com>
2024-12-25 15:36:24 +0100

    Add default vertex and fragment shaders

---
+++ shaders/default/default_fragment.glsl
@@ -0,0 +1,27 @@
+#version 460 core
+
+in vec2 fragCoord;          // Fragment Coordinate from 0 to iResolutin x or y
+in vec2 iResolution;        // Window resolution
+uniform float iTime;        // Elapsed time in seconds
+uniform float iTimeDelta;   // Frame time delta
+uniform int iFrame;         // Current frame number
+uniform vec4 iMouse;        // Mouse input (position and click)
+
+out vec4 fragColor;
+
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec2 uv = fragCoord.xy / iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0, 2, 4));
+
+    // Output to screen
+    fragColor = vec4(col, 1.0);
+}
+
+void main()
+{
+    mainImage(fragColor, fragCoord);
+}
